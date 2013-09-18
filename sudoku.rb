12dc440b4947668d5063a35ac7943e7d18ee8f3c
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-09-18 17:02:40 +0100

    Added solution page

--- sudoku.rb
+++ sudoku.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require_relative './lib/grid'
 require_relative './lib/cell'
 
+enable :sessions
+
 def random_sudoku
 	seed = (1..9).to_a.shuffle + Array.new(72, 0)
 	sudoku = Grid.new(seed.join)
@@ -9,7 +11,23 @@ def random_sudoku
 	sudoku.boxes_to_s.chars
 end
 
+def puzzle(sudoku)
+  sudoku = sudoku.dup
+	while sudoku.count { |value| value == " "} < 45
+		x = rand(1..81)
+		sudoku[x] = " "
+	end
+	sudoku
+end
+
 get '/' do
-	@current_solution = random_sudoku
-	erb :index
+	sudoku = random_sudoku
+  session[:solution] = sudoku
+  @current_solution = puzzle(sudoku)
+  erb :index
+end
+
+get '/solution' do
+	@current_solution = session[:solution]
+  erb :index
 end
